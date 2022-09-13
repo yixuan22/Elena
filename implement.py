e588e8eb3539e0926b6029df9670f8311dc8ffe1
Liuyi95 <113481900+Liuyi95@users.noreply.github.com>
2022-09-13 19:42:27 -0400

    Add files via upload

---
+++ implement.py
@@ -0,0 +1,16 @@
+First_number=input("First:")
+Second_number=input("Second:")
+Sum=float(First_number)+float(Second_number)
+print("Sum="+ str(Sum))
+
+Subtraction=float(First_number)-float(Second_number)
+print("Subtraction="+ str(Subtraction))
+
+Multiplication=float(First_number)*float(Second_number)
+print("Multiplication="+ str(Multiplication))
+
+Division=float(First_number)/float(Second_number)
+print("Division"+ str(Division))
+
+Cube=float(First_number)**float(Second_number)
+print("Cube"+ str(Cube))
